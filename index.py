facf6c4eb398d87beaec947615f0e7d139e8417d
Julien Drouart <julien.drouart.pro@gmail.com>
2022-10-08 18:58:40 +0200

    Ajout du tableau d'invaders et leurs mouvement vers le bas

--- index.py
+++ index.py
@@ -10,13 +10,15 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS score (highscore int)"
 cursor.execute(create_table)
 
+
 def insertDatabase(score):
     dbHighestScore = selectDatabase()
     if score > dbHighestScore:
         # create table in database
-        insert_query = "INSERT INTO score VALUES ("+str(score)+")"
+        insert_query = "INSERT INTO score VALUES (" + str(score) + ")"
         cursor.execute(insert_query)
         connection.commit()
+
 
 def selectDatabase():
     result = 0
@@ -28,11 +30,11 @@ def selectDatabase():
         highScore = 0
     return highScore
 
+
 pygame.init()
 
 screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption('SpaceInvader')
-
 
 run = True
 start = gameover = False
@@ -43,16 +45,32 @@ shipCooX = 500
 direction = "null"
 font = pygame.font.Font('freesansbold.ttf', 32)
 shipImg = pygame.image.load("ship.png")
+screen.blit(shipImg, (shipCooX, 730))
 score = rocketPosX = rocketPosY = 0
 clock = pygame.time.Clock()
 shipSpeed = 9
 highscore = selectDatabase()
 text2 = font.render('Highest Score : ' + str(highscore), True, (255, 255, 255))
-screen.blit(shipImg, (shipCooX, 730))
+
 screen.blit(text2, (700, 20))
 screen.blit(rocket, (rocketPosX, rocketPosY))
+beginText = font.render("Press any key to begin", True, (255, 255, 255))
+screen.blit(beginText, (350, 500))
 rocketState = "waiting"
 
+intervalOf10 = row = 0
+invaders = {}
+invaderImg = pygame.image.load("invader.png")
+
+for i in range(70):
+    invaders[i] = {'posX': (intervalOf10 * 100)+25, 'posY': (row * 50)+100, 'state': "alive"}
+    screen.blit(invaderImg, ((intervalOf10 * 100)+25, (row * 50)+100))
+
+    if intervalOf10 < 9:
+        intervalOf10 += 1
+    else:
+        intervalOf10 = 0
+        row += 1
 
 while run:
     while gameover:
@@ -88,14 +106,14 @@ while run:
                     direction = "left"
                 if event.key == pygame.K_RIGHT:
                     direction = "right"
-                if event.key == pygame.K_SPACE and rocketState=="waiting":
-                    rocketPosX = shipCooX+25
+                if event.key == pygame.K_SPACE and rocketState == "waiting":
+                    rocketPosX = shipCooX + 25
                     rocketPosY = 750
                     rocketState = "launching"
             if event.type == pygame.KEYUP:
                 direction = "null"
         if rocketState == "launching":
-            if rocketPosY<-50:
+            if rocketPosY < -50:
                 rocketState = "waiting"
             else:
                 rocketPosY -= 6
@@ -118,6 +136,9 @@ while run:
             screen.blit(rocket, (rocketPosX, rocketPosY))
             screen.blit(img, (20, 20))
             screen.blit(scoreText, (700, 20))
+            for i in range(70):
+                screen.blit(invaderImg, (invaders[i]["posX"],invaders[i]["posY"]))
+                invaders[i]["posY"]+=0.1
 
     clock.tick(60)
     pygame.display.flip()
